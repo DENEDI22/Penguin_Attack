c2990807e428a382f7a622838a71635a4e27fbe0
Daniel Liubymyi <denedi2004@gmail.com>
2021-02-16 05:06:04 +0200

    Basic movement added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float velocity;
+	private Rigidbody playerRb;
+
+	private void Awake()
+	{
+		playerRb = GetComponent<Rigidbody>();
+	}
+
+	public void Movement(InputAction.CallbackContext _ctx)
+	{
+		var _movementVector = _ctx.ReadValue<Vector2>();
+		Vector3 movementVector3 = new Vector3(_movementVector.x,0, _movementVector.y);
+		playerRb.velocity = (movementVector3 * velocity) + new Vector3(0, playerRb.velocity.y, 0);
+
+	}
+}
